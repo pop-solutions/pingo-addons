ffbd68bea6fcf2c289edb39e2b06d9c3cbb5fb01
Éder Brito <31638599+britoederr@users.noreply.github.com>
2019-12-29 23:26:07 -0300

    Update __manifest__.py

--- pt_importar_xml_nfe/__manifest__.py
+++ pt_importar_xml_nfe/__manifest__.py
@@ -5,23 +5,25 @@
     'summary': """
         Preencha automaticamente uma fatura de fornecedor baseado nos dados
         do XML da Nota Fiscal recebida.""",
-
     'description': """
+    xml
+    import
+    brazil
+    brasil
+    importar xml
+    importação de xml
+    nota fiscal
+    NFE
+    NFSE
     """,
-    'price': 59.0,
-    'currency': 'EUR',
     'author': "Pingo Tecnologia",
     'support': 'contato@pingotecnologia.com.br',
     'images': ['static/description/capa.jpg'],
     'website': "http://www.pingotecnologia.com",
 
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
     'category': 'Accounting & Finance',
     'version': '11.0.1',
 
-    # any module necessary for this one to work correctly
     'depends': ['base',
     'br_base',
     'br_account',
@@ -31,10 +33,11 @@
     'stock',
     'delivery'
     ],
+    'external_dependencies':{"python":[
+        'xmltodict',
+    ]},
 
-    # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
         'views/account.invoice.xml',
         'wizard/importar_xml_nfe.xml',
     ],
