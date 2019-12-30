6c3c1654f0d022d0db74eeacfbc359f4d3b99639
Éder Brito <31638599+britoederr@users.noreply.github.com>
2019-12-29 23:40:34 -0300

    Update __manifest__.py

--- pt_importar_xml_nfe/__manifest__.py
+++ pt_importar_xml_nfe/__manifest__.py
@@ -1,15 +1,21 @@
 # -*- coding: utf-8 -*-
 {
     'name': "Importar XML da NFe",
-    'license': 'AGPL-3',
+
     'summary': """
         Preencha automaticamente uma fatura de fornecedor baseado nos dados
         do XML da Nota Fiscal recebida.""",
-
     'description': """
+        xml
+        import
+        brazil
+        brasil
+        importar xml
+        importação de xml
+        nota fiscal
+        NFE
+        NFSE
     """,
-    'price': 99.0,
-    'currency': 'EUR',
     'author': "Pingo Tecnologia",
     'support': 'contato@pingotecnologia.com.br',
     'images': ['static/description/capa.jpg'],
@@ -27,11 +33,12 @@
     'stock',
     'delivery'
     ],
+    'external_dependencies':{"python":[
+        'xmltodict',
+    ]},
 
     'data': [
-        # 'security/ir.model.access.csv',
         'views/account.invoice.xml',
-        'views/account_invoice_line.xml',
         'wizard/importar_xml_nfe.xml',
     ],
     'installable': True,
